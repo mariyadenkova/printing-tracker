fd6744c5ec62dd8989e62e3d63e9eda36c0581ae
Mariya Denkova <denkova.md@gmail.com>
2018-01-10 12:43:07 +0200

    Address Controller - edited

--- src/main/java/eu/printingtrackerv2/controller/AddressController.java
+++ src/main/java/eu/printingtrackerv2/controller/AddressController.java
@@ -42,9 +42,9 @@ public class AddressController {
         return "address";
     }
 
-    @GetMapping("/address/customer/*{customId}")
-    public String getCustomerAddressesPage(Model model, @PathVariable Long customId) {
-        Set<AddressViewModel> addresses = this.addressService.findAllAddressesByCustomerId(customId);
+    @GetMapping("/address/customer/*{customerId}")
+    public String getCustomerAddressesPage(Model model, @PathVariable Long customerId) {
+        Set<AddressViewModel> addresses = this.addressService.findAllAddressesByCustomerId(customerId);
         model.addAttribute("addresses", addresses);
         return "address-customer";
     }
